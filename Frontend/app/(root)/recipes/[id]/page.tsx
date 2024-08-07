d43a29d23e0797985e59ae6f9ed92a9ced505684
hrushii <hrushiop@gmail.com>
2024-08-07 12:29:28 +0530

    Added recipe page

---
+++ Frontend/app/(root)/recipes/[id]/page.tsx
@@ -0,0 +1,228 @@
+import React from "react";
+import { IoShareOutline, IoBookmarkOutline } from "react-icons/io5";
+
+const Page = () => {
+  return (
+    <div>
+      <section className="py-12">
+        <div className="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+          <ol className="flex items-center whitespace-nowrap">
+            <li className="inline-flex items-center">
+              <a
+                className="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500"
+                href="#"
+              >
+                Home
+              </a>
+              <svg
+                className="shrink-0 mx-2 size-4 text-gray-400 dark:text-neutral-600"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m9 18 6-6-6-6"></path>
+              </svg>
+            </li>
+            <li className="inline-flex items-center">
+              <a
+                className="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500"
+                href="#"
+              >
+                App Center
+                <svg
+                  className="shrink-0 mx-2 size-4 text-gray-400 dark:text-neutral-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6"></path>
+                </svg>
+              </a>
+            </li>
+            <li
+              className="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-neutral-200"
+              aria-current="page"
+            >
+              Application
+            </li>
+          </ol>
+
+          <div className="container mx-auto flex flex-col md:flex-row mt-10">
+            <div className="w-full max-w-7xl mx-auto">
+              <div className="flex justify-end items-center gap-5">
+                <IoShareOutline className="size-7" />
+                <IoBookmarkOutline className="size-7" />
+              </div>
+
+              <h1 className="text-black font-manrope font-semibold text-3xl md:text-4xl lg:text-5xl leading-tight mb-8">
+                Strawberry Cream Cheesecake
+              </h1>
+              <div className="flex flex-wrap gap-7 mb-5 items-center">
+                <div className="flex items-center gap-x-3">
+                  <img
+                    className="size-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                    alt="Avatar"
+                  />
+                  <div>
+                    <h5 className="text-sm text-gray-800 dark:text-neutral-200">
+                      Lauren Waller
+                    </h5>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+                    />
+                  </svg>
+                  <div>
+                    <h5 className="text-sm text-gray-800 dark:text-neutral-200">
+                      Lauren Waller
+                    </h5>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+
+                  <div>
+                    <h5 className="text-sm text-gray-800 dark:text-neutral-200">
+                      15
+                    </h5>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6 text-yellow-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6 text-yellow-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6 text-yellow-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6 text-yellow-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6 text-yellow-500"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <hr />
+
+              <div className="img w-full my-5">
+                <img
+                  src="https://www.cubesnjuliennes.com/wp-content/uploads/2022/12/Tandoori-Chicken-Recipe-480x270.jpg"
+                  alt="Blog tailwind page"
+                  className="mb-8 rounded-2xl   w-full "
+                />
+              </div>
+              <div className="max-w-full">
+                <div className="flex divide-x divide-gray-300">
+                  <div className="px-4">
+                    <p>PREP TIME</p>
+                    <p>15 min</p>
+                  </div>
+                  <div className="px-4">
+                    <p>PREP TIME</p>
+                    <p>15 min</p>
+                  </div>
+                  <div className="px-4">
+                    <p>PREP TIME</p>
+                    <p>15 min</p>
+                  </div>
+                  <div className="px-4">
+                    <p>PREP TIME</p>
+                    <p>15 min</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Page;
